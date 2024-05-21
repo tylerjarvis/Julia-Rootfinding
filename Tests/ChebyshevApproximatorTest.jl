60fe481e765e31be0b77cdbc950cbc4bf32f6d4b
Nathan Longhurst <nathanlonghurst@gmail.com>
2024-05-21 12:28:48 -0600

    removed 'numpy' from documentation and added startedconverging unit tests with implementation

--- Tests/ChebyshevApproximatorTest.jl
+++ Tests/ChebyshevApproximatorTest.jl
@@ -1,82 +1,121 @@
 include("../../Julia-Rootfinding/yroots/ChebyshevApproximator.jl")
 
-function transformPointsTest()
-        # TESTING TRANSFORM
-        # 1 dim test
-        x_1 = [1;
-                -1;
-                .234775;
-                -1/3]
-        a_1 = [-5.54]
-        b_1 = [1.2]
-        result_1 = [1.2;
-        -5.54;
-        -1.37880825;
-        -3.293333333333333]
+function transformpoints_test()
+    """Tests if the transformpoints functions
+    correctly transforms points to the new bounds
+    """
+    # 1 dim test
+    x_1 = [1;
+            -1;
+            .234775;
+            -1/3]
+    a_1 = [-5.54]
+    b_1 = [1.2]
+    result_1 = [1.2;
+    -5.54;
+    -1.37880825;
+    -3.293333333333333]
 
-        @assert isapprox(transformPoints(x_1,a_1,b_1), result_1) "transform 1 dim failed"
+    @assert isapprox(transformpoints(x_1,a_1,b_1), result_1) "transform 1 dim failed"
 
-        # 1 dim test without arrays for upper/lower bounds
-        x_1 = [1;
-                -1;
-                .234775;
-                -1/3]
+    # 1 dim test without arrays for upper/lower bounds
+    x_1 = [1;
+            -1;
+            .234775;
+            -1/3]
         a_1 = -5.54
-        b_1 = 1.2
-        result_1 = [1.2;
-        -5.54;
-        -1.37880825;
-        -3.293333333333333]
+    b_1 = 1.2
+    result_1 = [1.2;
+                -5.54;
+                -1.37880825;
+                -3.293333333333333]
 
-        @assert isapprox(transformPoints(x_1,a_1,b_1), result_1) "transform 1 dim failed without array boxes"
+    @assert isapprox(transformpoints(x_1,a_1,b_1), result_1) "transform 1 dim failed without array boxes"
 
-        # 2 dim test
-        x_2 = [1 1;
-                -1 -1;
-                .234775 .3456;
-                -1/3 .99999999]
-        a_2 = [-5.54 0]
-        b_2 = [1.2 7.5]
-        result_2 = [1.2 7.5;
-                -5.54 0.0;
-                -1.37880825 5.046;
-                -3.293333333333333 7.4999999625]
-                
-        @assert isapprox(transformPoints(x_2,a_2,b_2), result_2) "transform 2 dim failed"
+    # 2 dim test
+    x_2 = [1 1;
+            -1 -1;
+            .234775 .3456;
+            -1/3 .99999999]
+    a_2 = [-5.54 0]
+    b_2 = [1.2 7.5]
+    result_2 = [1.2 7.5;
+            -5.54 0.0;
+            -1.37880825 5.046;
+            -3.293333333333333 7.4999999625]
+            
+    @assert isapprox(transformpoints(x_2,a_2,b_2), result_2) "transform 2 dim failed"
 
 
-        # 3 dim test
-        x_3 = [1 1 1;
-                -1 -1 -1;
-                -.2345555 .5 -.23455;
-                -1/3 .99999999 1]
-        a_3 = [0 0 1]
-        b_3 = [1.2 7.5 4]
-        result_3 = [1.2 7.5 4.;
-                0. 0. 1.;
-                0.4592667 5.625 2.148175;
-                0.4 7.4999999625 4.] 
+    # 3 dim test
+    x_3 = [1 1 1;
+            -1 -1 -1;
+            -.2345555 .5 -.23455;
+            -1/3 .99999999 1]
+    a_3 = [0 0 1]
+    b_3 = [1.2 7.5 4]
+    result_3 = [1.2 7.5 4.;
+            0. 0. 1.;
+            0.4592667 5.625 2.148175;
+            0.4 7.4999999625 4.] 
 
-        @assert isapprox(transformPoints(x_3,a_3,b_3), result_3) "transform 3 dim failed"
+    @assert isapprox(transformpoints(x_3,a_3,b_3), result_3) "transform 3 dim failed"
 
-        # 4 dim test
-        x_4 = [1 1 1 1;
-                -1 -1 -1 -1;
-                4/9 -.9 5/166 -5/8;
-                1/3 -9/10 1 -1]
-        a_4 = [-19 4 7 -8]
-        b_4 = [4/9 21/5 8 0]
-        result_4 = [0.44444444444444 4.2 8 0;
-                -19 4 7 -8;
-                -4.956790123456791263834020355716 4.01 7.515060240963855164864071412012 -6.5;
-                -6.037037037037038089692941866815 4.01 8 -8]
+    # 4 dim test
+    x_4 = [1 1 1 1;
+            -1 -1 -1 -1;
+            4/9 -.9 5/166 -5/8;
+            1/3 -9/10 1 -1]
+    a_4 = [-19 4 7 -8]
+    b_4 = [4/9 21/5 8 0]
+    result_4 = [0.44444444444444 4.2 8 0;
+            -19 4 7 -8;
+            -4.956790123456791263834020355716 4.01 7.515060240963855164864071412012 -6.5;
+            -6.037037037037038089692941866815 4.01 8 -8]
 
-        @assert isapprox(transformPoints(x_4,a_4,b_4), result_4) "transform 4 dim failed"
+    @assert isapprox(transformpoints(x_4,a_4,b_4), result_4) "transform 4 dim failed"
+end
+
+# function hasconverged_test()
+
+# end
+
+function getfinal_degree_test()
+
+end
+
+function startedconverging_test()
+    # .1 tolerance
+    # should return true
+    coefflist_1 = [3 2 1 .09999999999 .000456 .023 .01 .005]
+    tol_1 = .1
+    @assert startedconverging(coefflist_1,tol_1) == true ".1 tolerance that should have returned true that coefficients have started converging"
+
+    # .1 tolerance
+    # should return false
+    coefflist_2 = [11 5.23 1.34 .09999999999 .000456 .023 .1 .005]
+    tol_2 = .1
+    @assert startedconverging(coefflist_2,tol_2) == false ".1 tolerance that should have returned false that coefficients have started converging"
+
+    # .0000000000001 tolerance
+    # should return true
+    coefflist_3 = [75456 .0000000000000999978866666 .00000000000008726 .000000000000017382 .00000000000000327 .00000000000000019]
+    tol_3 = .0000000000001
+    @assert startedconverging(coefflist_3,tol_3) == true ".0000000000001 tolerance that should have returned true that coefficients have started converging"
+
+    # .0000000000001 tolerance
+    # should return false
+    coefflist_4 = [75456 .000000000000100000000000003829 .00000000000008726 .000000000000017382 .00000000000000327 .00000000000000019]
+    tol_4 = .0000000000001
+    @assert startedconverging(coefflist_4,tol_4) == false ".0000000000001 tolerance that should have returned false that coefficients have started converging"
 end
 
 # RUNNING ALL TESTS
-function allTests()
-        transformPointsTest()
+function alltests()
+        transformpoints_test()
+        # hasconverged_test()
+        getfinal_degree_test()
+        startedconverging_test()
 end
 
-allTests()
+alltests()
--- yroots/ChebyshevApproximator.jl
+++ yroots/ChebyshevApproximator.jl
@@ -1,4 +1,102 @@
-function transformPoints(x,a,b)
-    # python: ((b-a)*x+(b+a))/2
+function transformpoints(x,a,b)
+    """Transforms points from the interval [-1, 1] to the interval [a, b].
+
+    Parameters
+    ----------
+    x : array
+        The points to be tranformed.
+    a : array
+        The lower bounds on the interval.
+    b : array
+        The upper bounds on the interval.
+
+    Returns
+    -------
+    transformed_pts : array
+        The transformed points.
+    """
+
     return ((b-a).*x .+(b+a))/2
+end
+
+# function hasconverged(coeff,coeff2,tol)
+#     """Determine whether the high-degree coefficients of a Chebyshev approximation have converged
+#     to machine epsilon.
+
+#     Parameters
+#     ----------
+#     coeff : array
+#         Absolute values of chebyshev coefficients of degree n approximation.
+#     coeff2 : array
+#         Absolute values of chebyshev coefficients of degree 2n+1 approximation.
+#     tol : float
+#         Tolerance (distance from zero) used to determine wheher the coefficients have converged.
+
+#     Returns
+#     -------
+#     hasConverged : bool
+#         True if all the values of coeff and coeff2 are within tol of each other; False otherwise
+#     """
+# end
+
+function getfinal_degree(coeff,tol)
+    """Finalize the degree of Chebyshev approximation to use along one particular dimension.
+
+    This function is called after the coefficients have started converging at degree n. A degree
+    2n+1 approximation is passed in. Assuming that the coefficients have fully converged by degree 
+    3n/2, the cutoff epsVal is calculated as twice the max coefficient of degree at least 3n/2.
+    The final degree is then set as the largest coefficient with magnitude greater than epsVal.
+    
+    The rate of convergence is calculated assuming that the coefficients converge geometrically
+    starting from the largest coefficient until machine epsilon is reached. This is a lower bound, as
+    in practice, the coefficients usually slowly decrease at first but drop off fast at the end.
+
+    Parameters
+    ----------
+    coeff : array
+        Absolute values of chebyshev coefficients.
+    
+    Returns
+    -------
+    degree : int
+        The numerical degree of the approximation
+    epsVal : float
+        The epsilon value to which the coefficients have converged
+    rho : float
+        The geometric rate of convergence of the coefficients
+    """
+    # # Set the final degree to the position of the last coefficient greater than convergence value
+    # convergedDeg = int(3 * (len(coeff) - 1) / 4) # Assume convergence at degree 3n/2.
+    # epsVal = 2*np.max(coeff[convergedDeg:]) # Set epsVal to 2x the largest coefficient past degree 3n/2
+    # nonZeroCoeffs = np.where(coeff > epsVal)[0]
+    # degree = 1 if len(nonZeroCoeffs) == 0 else max(1, nonZeroCoeffs[-1])
+
+    # # Set degree to 0 for constant functions (all coefficients but first are less than tol)
+    # if np.all(coeff[1:] < tol):
+    #     degree = 0
+    
+    # # Calculate the rate of convergence
+    # maxSpot = np.argmax(coeff)
+    # if epsVal == 0: #Avoid divide by 0. epsVal shouldn't be able to shrink by more than 1e-24 cause floating point.
+    #      epsVal = coeff[maxSpot] * 1e-24
+    # rho = (coeff[maxSpot]/epsVal)**(1/((degree - maxSpot) + 1)) 
+    # return degree, epsVal, rho
+end
+
+function startedconverging(coefflist,tol)
+    """Determine whether the high-degree coefficients of a given Chebyshev approximation are near 0.
+
+    Parameters
+    ----------
+    coeffList : array
+        Absolute values of chebyshev coefficients.
+    tol : float
+        Tolerance (distance from zero) used to determine whether coeffList has started converging.
+    
+    Returns
+    -------
+    startedConverging : bool
+        True if the last 5 coefficients of coeffList are less than tol; False otherwise
+    """
+    return all(x -> x < tol, coefflist[end-4:end])
 end
