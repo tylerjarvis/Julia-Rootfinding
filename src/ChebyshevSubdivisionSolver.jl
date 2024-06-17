4c0ff734d6056170c39eac714f8ee3ce7a35f3c3
rowan-williams <rowanwilliams263@gmail.com>
2024-06-17 16:43:59 -0600

    implemented a few of the methods required for higher precision: twoSum, split, twoProd, twoProdWithSplit

---
+++ src/ChebyshevSubdivisionSolver.jl
@@ -0,0 +1,36 @@
+# TODO: import from a library like this one instead of crowding our sourcecode with pre-written code https://github.com/JeffreySarnoff/ErrorfreeArithmetic.jl/blob/main/src/sum.jl
+function twoSum(a,b)
+    """Returns x,y such that a+b=x+y exactly, and a+b=x in floating point."""
+    x = a+b
+    z = x-a
+    y = (a-(x-z)) + (b-z)
+    return x, y
+end
+
+# TODO: import from a library instead
+function split(a)
+    """Returns x,y such that a = x+y exactly and a = x in floating point."""
+    c = (2^27 + 1) * a
+    x = c-(c-a)
+    y = a-x
+    return x,y
+end
+
+# TODO: import from a library instead
+function twoProd(a,b)
+    """Returns x,y such that a*b=x+y exactly and a*b=x in floating point."""
+    x = a*b
+    a1,a2 = split(a)
+    b1,b2 = split(b)
+    y=a2*b2-(((x-a1*b1)-a2*b1)-a1*b2)
+    return x,y
+end
+
+# TODO: import from a library instead
+function TwoProdWithSplit(a,b,a1,a2)
+    """Returns x,y such that a*b = x+y exactly and a*b = x in floating point but with a already split."""
+    x = a*b
+    b1,b2 = split(b)
+    y=a2*b2-(((x-a1*b1)-a2*b1)-a1*b2)
+    return x,y
+end 
