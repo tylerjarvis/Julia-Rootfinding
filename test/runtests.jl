a2de71adbe90f138b45f4bb9a7bd1d4d1ac75ca5
JarvisRAs <ras4jarvis@gmail.edu>
2024-08-06 15:55:28 -0600

    Hopefully no changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -74,5 +74,5 @@ end
 # test_quadraticCheck2D()
 # test_quadraticCheck3D()
 # test_get_fixed_vars()
-test_quadraticCheckND()
-# test_quadraticCheck()
+# test_quadraticCheckND()
+test_quadraticCheck()
