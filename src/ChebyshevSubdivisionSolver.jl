1a1c9c711efd356ee507578c199e2558e10bd702
JarvisRAs <ras4jarvis@gmail.edu>
2024-08-27 12:51:20 -0600

    Probably no changes

--- src/ChebyshevSubdivisionSolver.jl
+++ src/ChebyshevSubdivisionSolver.jl
@@ -14,7 +14,7 @@ function twoSum(a,b)
 end
 
 # TODO: import from a library instead
-function split(a)
+function splitt(a)
     """Returns x,y such that a = x+y exactly and a = x in floating point."""
     c = (2^27 + 1) * a
     x = c-(c-a)
@@ -26,8 +26,8 @@ end
 function twoProd(a,b)
     """Returns x,y such that a*b=x+y exactly and a*b=x in floating point."""
     x = a .* b
-    a1,a2 = split(a)
-    b1,b2 = split(b)
+    a1,a2 = splitt(a)
+    b1,b2 = splitt(b)
     y=a2 .*b2-(((x-a1 .*b1)-a2 .*b1)-a1 .*b2)
     return x,y
 end
@@ -36,7 +36,7 @@ end
 function TwoProdWithSplit(a,b,a1,a2)
     """Returns x,y such that a*b = x+y exactly and a*b = x in floating point but with a already split."""
     x = a*b
-    b1,b2 = split(b)
+    b1,b2 = splitt(b)
     y=a2*b2-(((x-a1*b1)-a2*b1)-a1*b2)
     return x,y
 end 
